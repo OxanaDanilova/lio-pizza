580c340a41faa1639e96a12c283d83bd0a722d71
Ivan <i85chiosa@gmail.com>
2022-10-11 12:15:57 +0200

    About CSS

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,15 +1,31 @@
 import "./About.css"
 
 
-export default function About() { 
+export default function About() {
 
     return (
-        <div>
+        <div className="container">
             <h2>
-            Die Pizzeria Lio möchte Sie daran erinnern, dass die Pizzeria wegen Feiertagen vom 12.07 bis 11.08.geschlossen ist.La pizzeria Totò e Peppino vi vuole ricordare che la pizzeria rimarrà chiusa per ferie dal 12.07. Fino al 11.08.
+                <h1 className="lioPizza">Lio - PIZZA<br></br>Luay, Ivan und Oxana</h1> 
+                Gastronomie mit ausschließlich qualitativ hochwertigen Zutaten und Produkten aus eigenen Sortiment direktem draht zum Hersteller.
+                Das Resultat - echte authentische neapolitanische Pizza. <br></br>
+
+
+
+                Wir leben Pizza napoletana, wir brennen für unsere Produkte und wir sind stolz auf das, was wir geschaffen haben: Echte neapolitanische Pizza in Deutschland. Wir verwenden für unsere Pizza Napoletana ausschließlich 100% italienische Produkte, die wir direkt vom Hersteller in Italien importieren. Wir kennen die Hersteller und haben uns von der Qualität überzeugt. <br></br>
+
+                Denn nur die Verbindung aus den besten Produkten, Herzblut und Wissen kann zur besten neapolitanischen Pizza führen. Die Fior di Latte d'Agerola (Mozzarella) beispielsweise ist bereits 48 Stunden nach der Produktion in unserer Gastronomie angekommen und schmilzt geschwind bei 500 Grad im neapolitanischen Ofen ohne die Pizza nass zu machen. Wir bieten unseren Partnern die schnellste und frischeste Lieferkette von Italien nach Deutschland! Übrigens: Alle Produkte, die wir für unsere Pizza in der Antica Pizzeria Nennillo verwenden, sind 100% Made in Italy und erfüllen unsere hohen Ansprüche.<br></br>
+
+
+
+                Unser Herzstück, der mit Gas befeuerte Ofen, haben wir mit einem neapolitanischen Ofenbauer zusammen in Neapel selbst entwickelt. Wir lassen ihn dort von Hand anfertigen da wir mit den herkömmlichen und bekannten Öfen nicht ganz zufrieden sind. Mit diesem Ofen lässt sich die Pizza bei über 500 Grad backen – wie in den ältesten Pizzerien Neapels. <br></br>
+
+
+
+                Lio - erhält Traditionen - jedoch sind aktuelle Pizzatrends in Neapel nicht zu übersehen und wir arbeiten gemeinsam mit Pizzaioli in Neapel an neuen Ideen und Teigrezepten.  "Traditione e innovatione"
             </h2>
         </div>
 
     )
-    
+
 }
